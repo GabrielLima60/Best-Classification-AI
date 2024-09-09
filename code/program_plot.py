507d2203127f8b0545375f2b1a6957e9584f42f0
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2024-09-09 15:55:01 -0300

    Update program_plot.py

--- code/program_plot.py
+++ code/program_plot.py
@@ -11,17 +11,15 @@ def plot():
     metrics = ['F1-Score', 'Processing Time', 'ROC AUC', 'Memory Usage', 'Precision', 'Recall']
     num_metrics = len(metrics)
     
-    # Create subplots with one row per metric
-    fig, axes = plt.subplots(num_metrics, 1, figsize=(10, 6 * num_metrics))  # Adjust height based on number of metrics
+    fig, axes = plt.subplots(num_metrics, 1, figsize=(10, 6 * num_metrics))  
 
     for i, metric in enumerate(metrics):
         filtered_data = remove_outliers(dataset, metric)
         
-        # Create a dot plot for each metric in its corresponding subplot
         sns.stripplot(ax=axes[i], x='model', y=metric, hue='technique', data=filtered_data, jitter=True, size=7, palette='Set2')
         
-        # Add plot titles and labels
-        axes[i].set_title(f'Dot Plot for {metric} by Technique and Model (Without Outliers)')
+        axes[i].set_title(f'{metric} by Technique and Model (Without Outliers)')
+        axes[i].grid(True, axis='y', linestyle='--', alpha=0.7)
         axes[i].set_xlabel('Model')
         
         if metric == 'Memory Usage':
@@ -40,14 +38,12 @@ def plot():
     image_data = canvas.tostring_rgb()
     size = canvas.get_width_height()
 
-    # Save the entire figure as an image
     image = Image.frombytes("RGB", size, image_data)
     image.save('results image\\graphs.png')
 
 def remove_outliers(df, metric):
     mean = df[metric].mean()
     std = df[metric].std()
-    # Keep only the values within 3 standard deviations
     filtered_df = df[(df[metric] >= mean - 3 * std) & (df[metric] <= mean + 3 * std)]
     return filtered_df
 
